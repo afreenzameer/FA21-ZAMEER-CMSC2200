9139af78541d83afa721e1811403e253cbd4cd60
afreenzameer <90803406+afreenzameer@users.noreply.github.com>
2021-12-06 16:41:25 -0600

    Update Car.java

--- src/main/java/edu/ben/lab9/Car.java
+++ src/main/java/edu/ben/lab9/Car.java
@@ -1,7 +1,28 @@
 package edu.ben.lab9;
 
 public class Car {
-
+           private String make;
+           private String model;
+           private String color;
     public Car(String make, String model, String color) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+    }
+
+
+    public String toString(){
+        return "Make:" + this.make + ", Model:" + this.model + ", Color:" + this.color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
     }
 }
