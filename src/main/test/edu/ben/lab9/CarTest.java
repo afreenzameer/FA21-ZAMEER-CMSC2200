945daf01c97b66f1b1209cbd2f076f4ab99c9221
afreenzameer <90803406+afreenzameer@users.noreply.github.com>
2021-12-06 16:43:00 -0600

    Update CarTest.java

--- src/main/test/edu/ben/lab9/CarTest.java
+++ src/main/test/edu/ben/lab9/CarTest.java
@@ -1,6 +1,7 @@
 package edu.ben.lab9;
 
 import edu.ben.lab5.StringUtils;
+import edu.ben.lab7.Square;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,22 +9,57 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 public class CarTest {
+
+
     @ParameterizedTest
-    @MethodSource("provideCar")
-    void testCar(int Car, String[] expected) {
-//        String[] result = Car.class(Car);
-//        {
-//            assert result[input].equals(expected[input]);
-//        }
+    @MethodSource("provideMake")
+    void testMake(String make) {
+        Car s = new Car(make, "Civic", "Yellow");
+        assert s.getMake().equals(make);
     }
 
 
-    private static Stream<Arguments> provideCar() {
+    private static Stream<Arguments> provideMake() {
         return Stream.of(
-                Arguments.of(0, new String[]{"Honda", "Civic", "Yellow"}),
-                Arguments.of(1, new String[]{"Toyota", "Corolla", "Red"}),
-                Arguments.of(2, new String[]{"Dodge", "Prius", "Pink"})
+                Arguments.of("Honda"),
+                Arguments.of("Chrysler"),
+                Arguments.of("Ford")
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("provideModel")
+    void testModel(String model) {
+        Car s = new Car("Honda", model, "Yellow");
+        assert s.getModel().equals(model);
+    }
+
+
+    private static Stream<Arguments> provideModel() {
+        return Stream.of(
+                Arguments.of("Civic"),
+                Arguments.of("Corolla"),
+                Arguments.of("Prius")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideColor")
+    void testColor(String color) {
+        Car s = new Car("Honda", "Civic", color);
+        assert s.getColor().equals(color);
+    }
+
+
+    private static Stream<Arguments> provideColor() {
+        return Stream.of(
+                Arguments.of("Yellow"),
+                Arguments.of("Red"),
+                Arguments.of("Pink")
+        );
+    }
+
+
 }
+
+
